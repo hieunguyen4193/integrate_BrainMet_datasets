8bd40d3d4efe160ba099309c4efb2dc74de7d1d1
hieunguyen4193 <work@hieunguyen.de>
2024-11-20 01:17:03 +0100

    update

--- 13_integrate_all_cells_in_all_datasets.R
+++ 13_integrate_all_cells_in_all_datasets.R
@@ -16,7 +16,7 @@ all.integrated.metadata <- list(v0.1 = read.csv(file.path(path.to.main.src, "sam
 
 integrated.version <- "v0.2"
 
-outdir <- "/media/hieunguyen/HNSD01/outdir"
+outdir <- "/media/hieunguyen/GSHD_HN01/outdir"
 main.PROJECT <- "BrainMet_SeuratV5"
 
 path.to.main.output <- file.path(outdir, main.PROJECT, code.version, sprintf("integrated_%s", integrated.version))
@@ -103,3 +103,15 @@ writexl::write_xlsx(count.cancer.type, file.path(path.to.13.output, "num_cells_c
 count.samples <- table(merge17.metadata.cluster21$ID) %>% as.data.frame()
 writexl::write_xlsx(count.samples, file.path(path.to.13.output, "num_cells_samples_cluster21_GSE193745.xlsx"))
 
+##### Update 19.11.2024
+gene.list <- c("CD3E", "CD4", "CD8A", "NCAM1", "ITGAX", "CECAM8")
+DefaultAssay(s.obj.integrated) <- "SCT"
+feature.plot <- FeaturePlot(object = s.obj.integrated, reduction = "cca_UMAP", features = gene.list, label = TRUE, pt.size = 1) &
+  scale_color_gradient(low = "lightgray", high = "#FF0000", na.value = "lightgray")
+  # scale_color_distiller(palette = "RdBu")
+violin.plot <- VlnPlot(object = s.obj.integrated, features = gene.list, pt.size = 0, group.by = "cca.cluster.0.5")
+s.obj.integrated <- AddModuleScore(object = s.obj.integrated, features = list(gene.list), name = "check_gene_list_", ctrl = 50)
+
+feature.plot.module <- FeaturePlot(object = s.obj.integrated, reduction = "cca_UMAP", features = "check_gene_list_1", label = TRUE, pt.size = 1) &
+  scale_color_gradient(low = "lightgray", high = "#FF0000", na.value = "lightgray")
+violin.plot.module <- VlnPlot(object = s.obj.integrated, features = "check_gene_list_1", pt.size = 0, group.by = "cca.cluster.0.5")
