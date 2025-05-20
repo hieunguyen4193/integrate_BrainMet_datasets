4be165e63a717173b54b9a3d190f5a98e0a2d23a
hieunguyen4193 <work@hieunguyen.de>
2025-05-20 21:30:06 +0200

    update cell annotation scripts

--- 12a_recluster_with_different_resolutions.R
+++ 12a_recluster_with_different_resolutions.R
@@ -26,23 +26,31 @@ path.to.10.output <- file.path(path.to.main.output, "10_output")
 path.to.11.output <- file.path(path.to.main.output, "11_output")
 path.to.12.output <- file.path(path.to.main.output, "12_output")
 path.to.12a.output <- file.path(path.to.main.output, "12a_output")
+dir.create(path.to.12a.output, showWarnings = FALSE, recursive = TRUE)
 
-use.output <- "12a_output"
+print("Reading main seurat object data ...")
+s.obj <- readRDS(file.path(path.to.12.output, "s8_output", "integrated_BrainMet_dataset.output.s8.rds"))
 
-if (use.output == "12_output"){
-  s.obj <- readRDS(file.path(path.to.12.output, "s8_output", "integrated_BrainMet_dataset.output.s8.rds"))  
-} else if (use.output = "12a_output"){
-  
+selected.reduction <- "harmony"
+num.PCA <- 25
+num.PC.used.in.UMAP <- 25
+num.PC.used.in.Clustering <- 25
+regressOut_mode <- NULL
+features_to_regressOut <- NULL
+use.sctransform <- TRUE
+vars.to.regress <- c("percent.mt")
+
+if (file.exists(file.path(path.to.12a.output, "integrated_BrainMet_dataset.moreClusterRes.output.s8.rds")) == FALSE){
+  for (cluster.resolution in c(0.1, 0.2, 0.25, 0.3, 0.4)){
+    print("-----------------------------------------------------------")
+    print(sprintf("Working on clustering with resolution %s", cluster.resolution))
+    print("-----------------------------------------------------------")
+    s.obj <- FindNeighbors(s.obj, dims = 1:num.PC.used.in.Clustering, reduction = selected.reduction)
+    s.obj <- FindClusters(s.obj, resolution = cluster.resolution, cluster.name = sprintf("%s.cluster.%s", selected.reduction, cluster.resolution))  
+  }
+  saveRDS(s.obj, file.path(path.to.12a.output, "integrated_BrainMet_dataset.moreClusterRes.output.s8.rds")) 
+} else {
+  print("reading in saved data ...")
+  s.obj <- readRDS(file.path(path.to.12a.output, "integrated_BrainMet_dataset.moreClusterRes.output.s8.rds"))
 }
 
-path.to.17.output <- file.path(path.to.main.output, "17_output", "symphony", sprintf("from_%s", use.output))
-dir.create(path.to.17.output, showWarnings = FALSE, recursive = TRUE)
-
-# sanity check the data
-DimPlot(object = s.obj, reduction = "harmony_UMAP", label = TRUE, label.box = TRUE, group.by = "harmony.cluster.0.5")
-
-# if ("Azimuth" %in% installed.packages() == FALSE){
-  BiocManager::install(c("EnsDb.Hsapiens.v86c"), update = FALSE)
-  install.packages('/media/hieunguyen/GSHD_HN01/storage/offline_pkgs/BSgenome.Hsapiens.UCSC.hg38_1.4.5.tar.gz', type = "source", repos = NULL)
-  remotes::install_github('satijalab/azimuth', ref = 'master', upgrade = FALSE)
-# }
---
+++ scAno_01_dev.R
@@ -0,0 +1,68 @@
+gc()
+rm(list = ls())
+
+code.version <- "integrate_BrainMet_datasets"
+options(future.globals.maxSize = 10000 * 1024^2)
+PROJECT <- "integrated_BrainMet_dataset"
+
+path.to.pipeline.src <- "/media/hieunguyen/HNSD01/src/src_pipeline/scRNA_GEX_pipeline_SeuratV5"
+source(file.path(path.to.pipeline.src, "processes_src", "s8_integration_and_clustering_SeuratV5.R"))
+source(file.path(path.to.pipeline.src, "processes_src", "import_libraries.R"))
+source(file.path(path.to.pipeline.src, "processes_src", "helper_functions.R"))
+
+library(Matrix)
+
+path.to.main.src <- file.path("/media/hieunguyen/HNSD01/src/UKK/src/BrainMet/scRNAseq", code.version)
+all.integrated.metadata <- list(v0.1 = read.csv(file.path(path.to.main.src, "samples_to_integrated_20240513.csv")),
+                                v0.2 = read.csv(file.path(path.to.main.src, "samples_to_integrated_20240620.csv")))
+
+integrated.version <- "v0.2"
+
+outdir <- "/media/hieunguyen/GSHD_HN01/outdir"
+main.PROJECT <- "BrainMet_SeuratV5"
+
+path.to.main.output <- file.path(outdir, main.PROJECT, code.version, sprintf("integrated_%s", integrated.version), "scAnnotationPipeline")
+path.to.01.output <- file.path(path.to.main.output, "outdir")
+dir.create(path.to.01.output, showWarnings = FALSE, recursive = TRUE)
+
+path.to.s.obj <- "/media/hieunguyen/GSHD_HN01/outdir/BrainMet_SeuratV5/integrate_BrainMet_datasets/integrated_v0.2/12a_output/integrated_BrainMet_dataset.moreClusterRes.output.s8.rds"
+
+s.obj <- readRDS(path.to.s.obj)
+
+all.cluster.names <- to_vec(
+  for (item in colnames(s.obj@meta.data)){
+    if (grepl("harmony.cluster", item) == TRUE){
+      item
+    }
+  }
+)
+
+#####----------------------------------------------------------------------#####
+##### cell type annotation by find all clusters' markers
+#####----------------------------------------------------------------------#####
+if (file.exists(file.path(path.to.01.output, "check_FindAllMarkers.csv")) == FALSE){
+  min.pct <- 0.5
+  for (selected.cluster in all.cluster.names){
+    DefaultAssay(s.obj) <- "SCT"
+    s.obj <- PrepSCTFindMarkers(s.obj)
+    print("start running FindAllMarkers ...")
+    Idents(s.obj) <- selected.cluster
+    cluster.markers <- FindAllMarkers(object = s.obj, assay = "SCT", test.use = "wilcox", slot = "data", min.pct = min.pct, recorrect_umi = TRUE)
+    cluster.markers.raw <- cluster.markers
+    cluster.markers <- subset(cluster.markers, cluster.markers$p_val_adj < 0.05 & cluster.markers$avg_log2FC > 0)
+    saveRDS(cluster.markers, file.path(path.to.01.output, sprintf("cluster_markers_cluster_res_%s.rds", selected.cluster)))  
+    saveRDS(cluster.markers.raw, file.path(path.to.01.output, sprintf("cluster_markers_cluster_res_%s.raw.rds", selected.cluster)))  
+  }
+  write.csv(data.frame(status = c("check_finished_FindAllMarkers.csv")), file.path(path.to.01.output, "check_FindAllMarkers.csv"))
+} else {
+  print("reading saved data")
+  cluster.markers <- readRDS(file.path(path.to.01.output, sprintf("cluster_markers_cluster_res_%s.rds", selected.cluster)))
+  cluster.markers.raw <- readRDS(file.path(path.to.01.output, sprintf("cluster_markers_cluster_res_%s.raw.rds", selected.cluster)))  
+}
+
+
+#####----------------------------------------------------------------------#####
+##### annotation with SingleR and Celldex
+#####----------------------------------------------------------------------#####
+
+
